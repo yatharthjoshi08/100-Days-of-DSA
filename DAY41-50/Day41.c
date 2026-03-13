ec6ca54b428552039f4abb3a5232b10bf35f8731
yatharthjoshi08 <joshiyatharthh08@gmail.com>
2026-03-13 20:11:06 +0530

    Add files via upload

---
+++ DAY41-50/Day41.c
@@ -0,0 +1,84 @@
+// Problem Statement:
+// Implement a Queue using a linked list supporting enqueue and dequeue operations.
+
+// Input Format:
+// - First line contains integer N
+// - Next N lines contain queue operations
+
+// Output Format:
+// - Print dequeued elements
+// - Print -1 if dequeue is attempted on an empty queue
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+typedef struct {
+    Node* front;
+    Node* rear;
+} Queue;
+
+Queue* createQueue() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->front = NULL;
+    q->rear = NULL;
+    return q;
+}
+
+int isEmpty(Queue* q) {
+    return q->front == NULL;
+}
+
+void enqueue(Queue* q, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    if (isEmpty(q)) {
+        q->front = q->rear = newNode;
+    } else {
+        q->rear->next = newNode;
+        q->rear = newNode;
+    }
+}
+
+int dequeue(Queue* q) {
+    if (isEmpty(q)) {
+        return -1;
+    }
+    Node* temp = q->front;
+    int data = temp->data;
+    q->front = q->front->next;
+    if (q->front == NULL) {
+        q->rear = NULL;
+    }
+    free(temp);
+    return data;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    Queue* q = createQueue();
+    for (int i = 0; i < N; i++) {
+        char op[10];
+        scanf("%s", op);
+        if (strcmp(op, "enqueue") == 0) {
+            int val;
+            scanf("%d", &val);
+            enqueue(q, val);
+        } else if (strcmp(op, "dequeue") == 0) {
+            int res = dequeue(q);
+            printf("%d\n", res);
+        }
+    }
+    while (!isEmpty(q)) {
+        dequeue(q);
+    }
+    free(q);
+    return 0;
+}
