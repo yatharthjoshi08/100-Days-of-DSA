569a9e1e5adc1fd43bd0cf7559f4669c0885f6e0
yatharthjoshi08 <joshiyatharthh08@gmail.com>
2026-04-23 23:59:45 +0530

    Add files via upload

--- DAY81-90/Day81.c
+++ DAY81-90/Day81.c
@@ -1,74 +1,49 @@
-// Problem: Given a weighted graph with n vertices, implement the Floyd-Warshall algorithm to compute the shortest distances between every pair of vertices. The graph may contain positive or negative edge weights, but it does not contain any negative weight cycles.
+// Problem: Implement Bubble Sort - Implement the algorithm.
 
 // Input:
-// - First line: integer n representing the number of vertices
-// - Next n lines: n space-separated integers representing the adjacency matrix of the graph
-//   (use -1 to indicate no direct edge between two vertices)
+// - First line: integer n
+// - Second line: n space-separated integers
 
 // Output:
-// - Print the shortest distance matrix where the value at row i and column j represents the shortest distance from vertex i to vertex j
+// - Print the sorted array or search result
 
 // Example:
 // Input:
-// 4
-// 0 5 -1 10
-// -1 0 3 -1
-// -1 -1 0 1
-// -1 -1 -1 0
+// 5
+// 64 34 25 12 22
 
 // Output:
-// 0 5 8 9
-// -1 0 3 4
-// -1 -1 0 1
-// -1 -1 -1 0
-
-// Explanation:
-// The Floyd-Warshall algorithm works by considering each vertex as an intermediate point and updating the shortest paths between all pairs of vertices. For example, the shortest path from vertex 1 to vertex 3 is improved via vertex 2 with total cost 8. This process is repeated for all vertices, resulting in the final shortest distance matrix.
+// 12 22 25 34 64
 
 #include <stdio.h>
-#include <limits.h>
-
-#define INF 1000000000
 
 int main() {
     int n;
     scanf("%d", &n);
-
-    int dist[n][n];
-
+    int nums[10000];
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int x;
-            scanf("%d", &x);
-            if (x == -1) {
-                dist[i][j] = INF;
-            } else {
-                dist[i][j] = x;
+        scanf("%d", &nums[i]);
+    }
+    
+    // Bubble sort
+    for (int i = 0; i < n - 1; i++) {
+        int swapped = 0;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+                swapped = 1;
             }
         }
+        if (swapped == 0) break;
     }
-
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[i][k] != INF && dist[k][j] != INF
-                    && dist[i][k] + dist[k][j] < dist[i][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
-                }
-            }
-        }
-    }
-
+    
+    // Output
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (dist[i][j] == INF) {
-                printf("-1 ");
-            } else {
-                printf("%d ", dist[i][j]);
-            }
-        }
-        printf("\n");
+        printf("%d", nums[i]);
+        if (i < n - 1) printf(" ");
     }
-
+    printf("\n");
     return 0;
 }
