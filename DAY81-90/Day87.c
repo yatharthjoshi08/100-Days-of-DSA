b667c96b53fdffd33eb781ddd6c5111e1518a146
yatharthjoshi08 <joshiyatharthh08@gmail.com>
2026-04-28 20:57:29 +0530

    Add files via upload

---
+++ DAY81-90/Day87.c
@@ -0,0 +1,45 @@
+// Problem: Implement Binary Search Iterative - Implement the algorithm.
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+
+// Output:
+// - Print the sorted array or search result
+
+// Example:
+// Input:
+// 5
+// 64 34 25 12 22
+
+// Output:
+// 12 22 25 34 64
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[10000];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
